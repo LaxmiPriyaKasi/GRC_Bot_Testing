7bc7956ed50c99448bf79feaa0475519a24c871b
laxmipriyakasi@gmail.com <laxmipriyakasi@gmail.com>
2024-10-30 19:33:29 +0000

    Updated RAG Chatbot and Model Deployment Code

---
+++ RAG Chatbot/3 RAG Chatbot_updated.py
@@ -0,0 +1,105 @@
+# Databricks notebook source
+# DBTITLE 1,Install Dependencies
+# MAGIC %pip install mlflow==2.10.1 langchain==0.1.5 databricks-vectorsearch==0.22 databricks-sdk==0.18.0 mlflow[databricks]
+# MAGIC dbutils.library.restartPython()
+
+# COMMAND ----------
+
+# DBTITLE 1,Set needed parameters
+import os
+
+host = "https://" + spark.conf.get("spark.databricks.workspaceUrl")
+os.environ['DATABRICKS_TOKEN'] = dbutils.secrets.get(scope ="access-token", key = "my-token")
+
+index_name="llm.rag.docs_idx"
+host = "https://" + spark.conf.get("spark.databricks.workspaceUrl")
+
+VECTOR_SEARCH_ENDPOINT_NAME="grc_vector_search"
+
+# COMMAND ----------
+
+# DBTITLE 1,Build Retriever
+from databricks.vector_search.client import VectorSearchClient
+from langchain_community.vectorstores import DatabricksVectorSearch
+from langchain_community.embeddings import DatabricksEmbeddings
+
+embedding_model = DatabricksEmbeddings(endpoint="databricks-bge-large-en")
+
+def get_retriever(persist_dir: str = None):
+    os.environ["DATABRICKS_HOST"] = host
+    #Get the vector search index
+    vsc = VectorSearchClient(workspace_url=host, personal_access_token=os.environ["DATABRICKS_TOKEN"])
+    vs_index = vsc.get_index(
+        endpoint_name=VECTOR_SEARCH_ENDPOINT_NAME,
+        index_name=index_name
+    )
+
+    # Create the retriever
+    vectorstore = DatabricksVectorSearch(
+        vs_index, text_column="text", embedding=embedding_model
+    )
+    return vectorstore.as_retriever()
+
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Create the RAG Langchain
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+from langchain_community.chat_models import ChatDatabricks
+
+chat_model = ChatDatabricks(endpoint="databricks-dbrx-instruct", max_tokens = 200)
+
+TEMPLATE = """You are an expert in ISO standards and compliance. Based on the provided ISO document excerpts, answer the following question using specific information from the text. Provide concise, accurate, and factual answers, and cite the section or clause of the ISO standard where the information is found. If the answer is not directly available in the provided text, respond with 'Information not available.'.  Provide all answers only in English.
+use the following context: {context}
+Question: {question}
+Answer:
+"""
+prompt = PromptTemplate(template=TEMPLATE, input_variables=["context", "question"])
+
+chain = RetrievalQA.from_chain_type(
+    llm=chat_model,
+    chain_type="stuff",
+    retriever=get_retriever(),
+    chain_type_kwargs={"prompt": prompt}
+)
+
+
+
+# COMMAND ----------
+
+# DBTITLE 1,Test Langchain
+question = {"query": "What are the key requirements for maintaining quality management documentation?"}
+answer = chain.invoke(question)
+print(answer)
+
+# COMMAND ----------
+
+# DBTITLE 1,Register our Chain as a model to Unity Catalog
+from mlflow.models import infer_signature
+import mlflow
+import langchain
+
+mlflow.set_registry_uri("databricks-uc")
+model_name = "llm.rag.chatbot"
+
+with mlflow.start_run(run_name="appliance_chatbot_run") as run:
+    signature = infer_signature(question, answer)
+    model_info = mlflow.langchain.log_model(
+        chain,
+        loader_fn=get_retriever,  # Load the retriever with DATABRICKS_TOKEN env as secret (for authentication).
+        artifact_path="chain",
+        registered_model_name=model_name,
+        pip_requirements=[
+            "mlflow==" + mlflow.__version__,
+            "langchain==" + langchain.__version__,
+            "databricks-vectorsearch",
+        ],
+        input_example=question,
+        signature=signature
+    )
+
+# COMMAND ----------
+
+
